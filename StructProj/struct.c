54792c7cb4d9ac52ec52f4ed452817b604553959
황인찬 <neogeoss1@gmail.com>
2018-12-01 21:49:00 +0900

    C struct example

--- StructProj/struct.c
+++ StructProj/struct.c
@@ -1,7 +1,22 @@
 #include<stdio.h>
 
+struct Student {
+	char studentId[10];
+	char name[10];
+	int grade;
+	char major[100];
+} s ;
+
 int main() {
-	printf("Hello World\n");
+	
+	strcpy(s.studentId, "931060635");
+	strcpy(s.name, "황인찬");
+	s.grade = 4;
+	strcpy(s.major, "소프트웨어 공학과");
+	printf("%s\n", s.studentId);
+	printf("%s\n", s.name);
+	printf("%s\n", s.major);
+	printf("%d\n", s.grade);
 	system("pause");
 	return 0;
 }
